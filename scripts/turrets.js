2459a64f68072238946bc83ac8f2b7166643a54f
Duck <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-06-05 22:26:10 -0700

    Update turrets.js

--- scripts/turrets.js
+++ scripts/turrets.js
@@ -6,4 +6,17 @@ const fusor = extend(PowerTurret, "fusor", {});
 
 const thearchy = extend(PowerTurret, "thearchy", {});
 
-const blade = extend(PowerTurret, "blade", {});
+const bladeBeam = extend(LaserBulletType, {
+  	length: 200,
+  	colors: [Color.valueOf("989aa4"), Color.valueOf("dcdde3"), Color.white],
+  	damage: 150,
+  	hitSize: 4,
+  	drawSize: 400,
+  	lifetime: 35,
+  	sideAngle: 15,
+  	width: 32,
+});
+
+const blade = extend(PowerTurret, "blade", {
+  shootType: bladeBeam,
+});
