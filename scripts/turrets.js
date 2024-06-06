c514aba54fbb3e5decaf4e4a7fcf95e898cf1025
Duck <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-06-05 22:13:51 -0700

    Create turrets.js

---
+++ scripts/turrets.js
@@ -0,0 +1,7 @@
+const fungi = extend(ItemTurret, "fungi", {});
+
+const vortex = extend(ItemTurret, "vortex", {});
+
+const fusor = extend(ItemTurret, "fusor", {});
+
+const thearchy = extend(ItemTurret, "thearchy", {});
