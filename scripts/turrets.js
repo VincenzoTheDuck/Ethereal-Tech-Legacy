74f6334957f02c205ebedd2656f7c28fc0c10fac
Duck <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-06-05 22:23:40 -0700

    Update turrets.js

--- scripts/turrets.js
+++ scripts/turrets.js
@@ -2,6 +2,8 @@ const fungi = extend(ItemTurret, "fungi", {});
 
 const vortex = extend(ItemTurret, "vortex", {});
 
-const fusor = extend(ItemTurret, "fusor", {});
+const fusor = extend(PowerTurret, "fusor", {});
 
-const thearchy = extend(ItemTurret, "thearchy", {});
+const thearchy = extend(PowerTurret, "thearchy", {});
+
+const blade = extend(PowerTurret, "blade", {});
