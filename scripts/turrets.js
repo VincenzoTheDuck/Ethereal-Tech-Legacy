e1b015d271313bc0b3f81853553844cc4ae2f8dd
Duck <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-06-05 22:34:45 -0700

    Update turrets.js

--- scripts/turrets.js
+++ scripts/turrets.js
@@ -6,6 +6,14 @@ const fusor = extend(PowerTurret, "fusor", {});
 
 const thearchy = extend(PowerTurret, "thearchy", {});
 
+const bladeShoot = new Effect(20, e => {
+	Draw.color(Color.white, Color.valueOf("989aa4");, e.fin());
+
+  Drawf.tri(e.x, e.y, 6, 60 * e.fout(), e.rotation-30);
+  Drawf.tri(e.x, e.y, 6, 60 * e.fout(), e.rotation);
+  Drawf.tri(e.x, e.y, 6, 60 * e.fout(), e.rotation+30);
+});
+
 const bladeBeam = extend(LaserBulletType, {
   	length: 200,
   	colors: [Color.valueOf("989aa4"), Color.valueOf("dcdde3"), Color.white],
