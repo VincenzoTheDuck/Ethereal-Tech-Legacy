b988fc5919022e63539ffbb7f39b74c5e5443d9f
Duck <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-06-06 07:48:21 -0700

    Update turrets.js

--- scripts/blocks/turrets.js
+++ scripts/blocks/turrets.js
@@ -7,13 +7,17 @@ const fusor = extend(PowerTurret, "fusor", {});
 const thearchy = extend(PowerTurret, "thearchy", {});
 
 const bladeShoot = new Effect(20, e => {
+	let p = 4
+	
   	Draw.color(Color.white, Color.valueOf("989aa4"), e.fin());
 	
   	const hl = new Floatc2({get: function(x, y){
-  		Fill.poly(e.x + x, e.y + y, 3, e.fout() * 9, e.rotation+Mathf.random(-180,180));
+		Mathf.rand.setSeed(e.id)
+		
+  		Fill.poly(e.x + x, e.y + y, 3, e.fout() * 5, e.rotation+Mathf.random(-180,180));
   	}});
 	
-  	Angles.randLenVectors(e.id, 4, e.finpow() * 20.0, e.rotation, 180.0, hl);
+  	Angles.randLenVectors(e.id, p, e.finpow() * 35.0, e.rotation, 180.0, hl);
 });
 
 const bladeBeam = extend(LaserBulletType, {
