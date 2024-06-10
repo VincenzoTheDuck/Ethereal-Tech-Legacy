be981edbee4f16c970e04dbe455883f8ed040087
Duck <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-06-10 11:42:30 -0700

    Update crysola.js

--- scripts/campaign/crysola.js
+++ scripts/campaign/crysola.js
@@ -8,7 +8,11 @@ crysola.startSector = 13;
 crysola.hasAtmosphere = true;
 crysola.alwaysUnlocked = true;
 crysola.atmosphereColor = Pal.lancerLaser;
-crysola.meshLoader = new HexMesh(crysola, 5)
+crysola.meshLoader = new HexMesh(crysola, 6)
 crysola.atmosphereRadIn = -0.02;
 crysola.atmosphereRadOut = 0.12;
 crysola.allowLaunchToNumbered = false;
+crysola.cloudMeshLoader = new MultiMesh(
+  new HexSkyMesh(crysola, 11, 0.15, 0.13, 5, Color.valueOf("a4b8fa85"), 2, 0.45, 0.9, 0.38),
+  new HexSkyMesh(crysola, 1, 0.6, 0.16, 5, Color.valueOf("a4b8fa90"), 2, 0.45, 1, 0.41)
+);
