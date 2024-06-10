a24c2b0c37084ea63c605d12e6a96176d07d73e2
Duck <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-06-10 11:05:31 -0700

    gee i hope this works

--- scripts/campaign/crysola.js
+++ scripts/campaign/crysola.js
@@ -1,1 +1,14 @@
+// wip
 
+const crysolaGen = extend(PlanetGenerator, {});
+
+const crysola = new JavaAdapter(Planet, {}, "crysola", Planets.sun, 1, 2.5)
+crysola.generator = crysolaGen;
+crysola.startSector = 13;
+crysola.hasAtmosphere = true;
+crysola.alwaysUnlocked = true;
+crysola.atmosphereColor = Pal.lancerLaser;
+crysola.meshLoader = new HexMesh(crysola, 5)
+crysola.atmosphereRadIn = -0.02;
+crysola.atmosphereRadOut = 0.12;
+crysola.allowLaunchToNumbered = false;
